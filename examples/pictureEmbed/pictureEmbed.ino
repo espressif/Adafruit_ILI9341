88dc3b40370cc01cf61a34db9b210ca5f04245b1
Marc MERLIN <marc_soft@merlins.org>
2017-03-05 07:02:29 -0800

    converted to updated adafruit lib, added bitmap and scroll example.

--- examples/pictureEmbed/pictureEmbed.ino
+++ examples/pictureEmbed/pictureEmbed.ino
@@ -8,11 +8,15 @@
 #include "SPI.h"
 #include <Adafruit_ILI9341.h>
 
-
-// Converted to code with:
+// New picture format, as converted by 
 // http://www.rinkydinkelectronics.com/t_imageconverter565.php
-//
-#include "picture.c" //the picture
+#include "picture1.c"
+
+// Original picture convertion, with a header to skip (from Frank Bösing)
+#include "picture2.c"
+
+// My own logo for the IoTuz board (rolled out in Tasmania)
+#include "tasmanian-devil.c"
 
 /* GIMP (https://www.gimp.org/) can also be used to export the image using the following steps:
 
@@ -27,11 +31,10 @@
   Assuming 'image_name' was typed in the 'prefixed name' box of step 4, you can have to include the c file as above,
   using the image can be done with:
 
-    tft.writeRect(0, 0, image_name.width, image_name.height, (uint16_t*)(image_name.pixel_data));
+    tft.drawBitmap(0, 0, image_name.width, image_name.height, (uint16_t*)(image_name.pixel_data));
 
   See also https://forum.pjrc.com/threads/35575-Export-for-ILI9341_t3-with-GIMP 
 */
-
 
 // Normal Connections
 #define TFT_DC       9
@@ -49,15 +52,27 @@
 //#define TFT_SCLK    14
 //#define TFT_MISO    12
 
-ILI9341_t3 tft = ILI9341_t3(TFT_CS, TFT_DC, TFT_RST, TFT_MOSI, TFT_SCLK, TFT_MISO);
+//Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST, TFT_MOSI, TFT_SCLK, TFT_MISO);
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+
 
 void setup() {
   tft.begin();
   tft.setRotation(3);
   tft.fillScreen(ILI9341_BLACK);
-  tft.writeRect(32, 33, 256, 174, (uint16_t*)picture);
+  tft.drawBitmap(33, 32, 256, 174, (uint16_t*)picture1);
+  delay(1000);
+  tft.fillScreen(ILI9341_BLACK);
+  tft.drawBitmap(1, 1, 256, 174, (uint16_t*)picture2+35);
+  delay(2000);
+  tft.drawBitmap(0, 0, 320, 240, (uint16_t*)picture3);
+  delay(2000);
+  for (int16_t addr = 320; addr -=2; addr >= 0)
+  {
+    tft.scrollTo(addr);
+    delay(10);
+  }
 }
 
 void loop(void) {
 }
-
